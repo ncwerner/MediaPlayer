2e9dce6c1382b38cbe188e808c1d9ccc49cb3c41
rjdenney67 <rdenney@ufl.edu>
2018-02-25 14:19:45 -0500

    Add files via upload

--- BearPlayer/BearPlayer/BearPlayer.Designer.cs
+++ BearPlayer/BearPlayer/BearPlayer.Designer.cs
@@ -28,12 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Artists");
-            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Albums");
-            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Songs");
-            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("New Playlist");
-            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Playlists", new System.Windows.Forms.TreeNode[] {
-            treeNode4});
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Artists");
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Albums");
+            System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Songs");
+            System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("New Playlist");
+            System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Playlists", new System.Windows.Forms.TreeNode[] {
+            treeNode9});
             this.MenuBar = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newPlaylistToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -143,7 +143,6 @@
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
             this.exitToolStripMenuItem.Size = new System.Drawing.Size(155, 22);
             this.exitToolStripMenuItem.Text = "Exit";
-            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click_1);
             // 
             // editToolStripMenuItem
             // 
@@ -329,6 +328,7 @@
             this.volumeUpToolStripMenuItem.Name = "volumeUpToolStripMenuItem";
             this.volumeUpToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
             this.volumeUpToolStripMenuItem.Text = "Volume Up ";
+            this.volumeUpToolStripMenuItem.Click += new System.EventHandler(this.volumeUpToolStripMenuItem_Click);
             // 
             // volumeDownToolStripMenuItem
             // 
@@ -366,26 +366,26 @@
             this.treeView1.ItemHeight = 30;
             this.treeView1.Location = new System.Drawing.Point(0, 25);
             this.treeView1.Name = "treeView1";
-            treeNode1.Name = "Artists";
-            treeNode1.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            treeNode1.Text = "Artists";
-            treeNode2.Name = "Albums";
-            treeNode2.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            treeNode2.Text = "Albums";
-            treeNode3.Name = "Songs";
-            treeNode3.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            treeNode3.Text = "Songs";
-            treeNode4.Name = "New Playlist";
-            treeNode4.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            treeNode4.Text = "New Playlist";
-            treeNode5.Name = "Playlists";
-            treeNode5.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            treeNode5.Text = "Playlists";
+            treeNode6.Name = "Artists";
+            treeNode6.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode6.Text = "Artists";
+            treeNode7.Name = "Albums";
+            treeNode7.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode7.Text = "Albums";
+            treeNode8.Name = "Songs";
+            treeNode8.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode8.Text = "Songs";
+            treeNode9.Name = "New Playlist";
+            treeNode9.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode9.Text = "New Playlist";
+            treeNode10.Name = "Playlists";
+            treeNode10.NodeFont = new System.Drawing.Font("MS Reference Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            treeNode10.Text = "Playlists";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
-            treeNode1,
-            treeNode2,
-            treeNode3,
-            treeNode5});
+            treeNode6,
+            treeNode7,
+            treeNode8,
+            treeNode10});
             this.treeView1.PathSeparator = "";
             this.treeView1.ShowLines = false;
             this.treeView1.ShowPlusMinus = false;
@@ -477,6 +477,8 @@
             this.volumeSlider.TabIndex = 0;
             this.volumeSlider.TickFrequency = 0;
             this.volumeSlider.TickStyle = System.Windows.Forms.TickStyle.None;
+            this.volumeSlider.Value = 100;
+            this.volumeSlider.Scroll += new System.EventHandler(this.volumeSlider_Scroll_1);
             // 
             // path
             // 
--- BearPlayer/BearPlayer/BearPlayer.cs
+++ BearPlayer/BearPlayer/BearPlayer.cs
@@ -14,6 +14,8 @@ namespace BearPlayer
     public partial class BearPlayer : Form
     {
         bool play;   // Global variable for controling play/pause state
+        string file_path;   // directory of chosed song
+        WMPLib.WindowsMediaPlayer Player;   //player object from WMP library
 
         public BearPlayer()
         {
@@ -21,6 +23,7 @@ namespace BearPlayer
 
             //this.WindowState = System.Windows.Forms.FormWindowState.Maximized;   // Opens application maximized
             InitializeComponent();
+            Player = new WMPLib.WindowsMediaPlayer();
         }
 
         private void BearPlayer_Load(object sender, EventArgs e)
@@ -31,11 +34,21 @@ namespace BearPlayer
         private void pictureBox1_Click(object sender, EventArgs e)
         {
             if (play)
+            {
                 this.playBar.Image = Image.FromFile(Application.StartupPath + "\\" + "pauseButton.png");
-
+                if (Player.URL != file_path)
+                {
+                    Player.URL = file_path;
+                }
+                //sets the URL for the player as the file path
+                Player.controls.play();
+                //plays the file that is currently set as the URL
+            }
             else
+            {
                 this.playBar.Image = Image.FromFile(Application.StartupPath + "\\" + "playButton1.png");
-
+                Player.controls.pause(); // SHOULD BE CHANGED TO PAUSE EVENTUALLY BUT CURRENTLY PAUSE CAUSES IT TO REPEAT IMMEDIATELY
+            }
             play = !play;
         }
 
@@ -43,41 +56,55 @@ namespace BearPlayer
         {
 
             string folder_path = "";
+            //initializes the folder path
             using (var folderDialog = new FolderBrowserDialog())
             {
                 if (folderDialog.ShowDialog() == DialogResult.OK)
                 {
 
                     folder_path = folderDialog.SelectedPath;
+                    //saves the selected folder as folder path
                 }
             }
             path.Text = folder_path;
+            //shows the path in the text box
         }
 
         private void importSongToolStripMenuItem_Click(object sender, EventArgs e)
         {
 
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            //creates a new file dialog each time the inport song button is clicked
             openFileDialog1.Filter = "Music Files| *.mp3";
+            //sets the filter to only show mp3 files
             openFileDialog1.Title = "Select a Song";
-            string file_path = "";
+            file_path = "";
+            //initializes the file path
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
 
                 file_path = openFileDialog1.FileName;
-
             }
             path.Text = file_path;
+            //set the path that is saved to be the text for the textbox in the gui
+
         }
-        
+
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
         }
 
-        private void exitToolStripMenuItem_Click_1(object sender, EventArgs e)
+
+        private void volumeUpToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            this.Close();
+
+        }
+
+        private void volumeSlider_Scroll_1(object sender, EventArgs e)
+        {
+            Player.settings.volume = volumeSlider.Value;
+            //path.Text = (volumeSlider.Value).ToString();
         }
     }
 }
