5b103071a6505afe215b82d4bc40b1ff2aa577cd
Daniel Gilbert <dangilbert98@gmail.com>
2018-03-22 15:23:22 -0400

    shuffle toggle buttons

--- BearPlayer/BearPlayer/Properties/Resources.Designer.cs
+++ BearPlayer/BearPlayer/Properties/Resources.Designer.cs
@@ -159,5 +159,15 @@ namespace BearPlayer.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap shuffleButtonOff {
+            get {
+                object obj = ResourceManager.GetObject("shuffleButtonOff", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
